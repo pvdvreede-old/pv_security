f61b4f9094b429b9d10637072a772b962ed4d2a1
Paul Van de Vreede <paul@vdvreede.net>
2011-08-26 20:21:08 +1000

    Added check before inserting record. Changed join to only occur for non logged in users.

--- pv_security.php
+++ pv_security.php
@@ -26,13 +26,13 @@ function pvs_install() {
     global $wpdb;
 
     $sql = "CREATE TABLE " . PV_SECURITY_TABLENAME . " (
-      id mediumint(9) NOT NULL AUTO_INCREMENT,
-      role mediumint(9) NOT NULL,
+      ID mediumint(9) NOT NULL AUTO_INCREMENT,
+      role varchar(25) NOT NULL,
       object_id mediumint(9) NOT NULL,
       object_type varchar(25) NOT NULL,
       created_date datetime DEFAULT '0000-00-00 00:00:00' NOT NULL,
       modified_date datetime NULL,
-	  PRIMARY KEY  id (id)
+	  PRIMARY KEY  id (ID)
 	);";
 
     require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
@@ -61,18 +61,18 @@ function pvs_add_post_meta_box() {
     }
 }
 
-function pvs_render_post_security_meta_box( $post ) {
+function pvs_render_post_security_meta_box($post) {
 
     // Use nonce for verification
     wp_nonce_field(plugin_basename(__FILE__), 'pv_security_noncename');
 
     $output = '<p>Select which user roles can see this post. Not selecting any means the whole world can see it.</p>';
-    
+
     $roles = array(
         'public',
         'members'
     );
-    
+
     foreach ($roles as $role) {
         $output .= '<p><input type="radio" name="pv_security_role" value="' . $role . '" />  ' . ucfirst($role) . '</p>';
     }
@@ -90,17 +90,15 @@ function pvs_save_post_security_data($post_id) {
     if (!wp_verify_nonce($_POST['pv_security_noncename'], plugin_basename(__FILE__)))
         return;
 
-    foreach ($_POST['pv_security_role'] as $role) {
-
-        pvs_save_post_security($post_id, $role, 'post');
-    }
+    if ($_POST['pv_security_role'] == 'members')
+        pvs_save_post_security($post_id, 'members', 'post');
 }
 
 function pvs_delete_post_security_data($post_id) {
     global $wpdb;
 
-    $sql = "DELETE FROM " . PV_SECURITY_TABLENAME . " WHERE " . PV_SECURITY_TABLENAME . ".object_id = " . $post_id . " ";
-    $sql .= "AND " . PV_SECURITY_TABLENAME . ".object_type = 'post' ";
+    $sql = "DELETE FROM " . PV_SECURITY_TABLENAME . " as pvs WHERE pvs.object_id = " . $post_id . " ";
+    $sql .= "AND pvs.object_type = 'post' ";
 
     $sql = $wpdb->prepare($sql);
 
@@ -110,29 +108,25 @@ function pvs_delete_post_security_data($post_id) {
 function pvs_save_post_security($object_id, $role, $object_type) {
     global $wpdb;
 
-    $wpdb->insert(PV_SECURITY_TABLENAME, array(
-        'object_id' => $object_id,
-        'role' => $role,
-        'object_type' => $object_type,
-        'created_date' => date('Y-m-d H:m:s')
-    ));
+    $count = $wpdb->get_var($wpdb->prepare("SELECT COUNT(*) FROM " . PV_SECURITY_TABLENAME . " as pvs WHERE pvs.object_id = " . $object_id . " AND pvs.object_type = 'post'"));
+
+    if ($count == 0) {
+        $wpdb->insert(PV_SECURITY_TABLENAME, array(
+            'object_id' => $object_id,
+            'role' => $role,
+            'object_type' => $object_type,
+            'created_date' => date('Y-m-d H:m:s')
+        ));
+    }
 }
 
 function pvs_join_security($join) {
     global $wpdb;
 
-    if (is_user_logged_in()) {
-
-        $current_user = wp_get_current_user();
-        $role = $current_user->roles[0];
-    } else {
-
-        $role = 'anonymous';
+    if (!is_user_logged_in()) {
+        $join .= " RIGHT JOIN " . PV_SECURITY_TABLENAME . " pvs ON " . $wpdb->posts . ".ID = pvs.object_id ";
+        $join .= "AND pvs.object_type = 'post' ";
     }
-
-    $join .= " RIGHT JOIN " . PV_SECURITY_TABLENAME . " ON " . $wpdb->posts . ".ID = " . PV_SECURITY_TABLENAME . ".object_id ";
-    $join .= "AND " . PV_SECURITY_TABLENAME . ".object_type = 'post' ";
-    $join .= "AND " . PV_SECURITY_TABLENAME . ".role = '" . $role . "' ";
 
     return $join;
 }
