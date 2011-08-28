1a9e94433b9e3ce1cb906385d87bc68a88d9aa58
Paul Van de Vreede <paul@vdvreede.net>
2011-08-28 21:02:18 +1000

    Changed content type for images so they arent downloaded directly.

--- download.php
+++ download.php
@@ -1,35 +1,51 @@
 <?php
+
 /**
  * This file is used to shield files from unauthorised users
  */
-
-if (!isset ($_GET['filename']))
-    die ('Must supply a filename.');
+if (!isset($_GET['filename']))
+    die('Must supply a filename.');
 
 $filename = $_GET['filename'];
 
 require_once(dirname(__FILE__) . '/../../../wp-load.php');
 
 if (is_user_logged_in()) {
-    
+
     // If the user is logged in they can download any files, so dont check anymore.
-    
     // get the uploads path to attach to the filename
     $middle_path = get_option('upload_path');
-    
+
     $full_filename = ABSPATH . $middle_path . $filename;
+
+    // if its an image file then we want to display it, not download it, so change the header
+    $extension = end(explode(".", basename($filename)));
+
+    $image_extensions = array(
+        'png',
+        'jpg',
+        'gif'
+    );
+    
     
     header("Pragma: public"); // required 
-    header("Expires: 0"); 
-    header("Cache-Control: must-revalidate, post-check=0, pre-check=0"); 
-    header("Cache-Control: private",false); // required for certain browsers 
-    header("Content-Type: application/force-download"); 
-    header("Content-Disposition: attachment; filename=\"".basename($filename)."\";" ); 
-    header("Content-Transfer-Encoding: binary"); 
-    header("Content-Length: ".filesize($full_filename)); 
-    readfile("$full_filename");    
+    header("Expires: 0");
+    header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
+    header("Cache-Control: private", false); // required for certain browsers 
+
+    if (in_array($extension, $image_extensions)) {
+        header("Content-Type: image/". $extension);
+    } else {
+        header("Content-Type: application/force-download");
+        header("Content-Disposition: attachment; filename=\"" . basename($filename) . "\";");
+        header("Content-Transfer-Encoding: binary");
+    }
+    header("Content-Length: " . filesize($full_filename));
+
+    readfile("$full_filename");
 } else {
-    header('Location: '. get_bloginfo('url') . '/wp-login.php');
+    // If the user isnt logged in, then redirect the site to the login page.
+    header('Location: ' . get_bloginfo('url') . '/wp-login.php');
 }
 
 
