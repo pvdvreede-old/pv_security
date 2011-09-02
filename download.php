02b714c1212be288ae1ede8768d47e9b36a6b32f
Paul Van de Vreede <paul@vdvreede.net>
2011-09-02 12:45:00 +1000

    Made change to download check to allow download of files not attached to private posts.

--- download.php
+++ download.php
@@ -10,7 +10,7 @@ $filename = $_GET['filename'];
 
 require_once(dirname(__FILE__) . '/../../../wp-load.php');
 
-if (is_user_logged_in()) {
+if (is_user_logged_in() || (!is_user_logged_in() && is_file_secure($filename))) {
 
     // If the user is logged in they can download any files, so dont check anymore.
     // get the uploads path to attach to the filename
@@ -26,8 +26,7 @@ if (is_user_logged_in()) {
         'jpg',
         'gif'
     );
-    
-    
+     
     header("Pragma: public"); // required 
     header("Expires: 0");
     header("Cache-Control: must-revalidate, post-check=0, pre-check=0");
@@ -46,6 +45,25 @@ if (is_user_logged_in()) {
 } else {
     // If the user isnt logged in, then redirect the site to the login page.
     header('Location: ' . get_bloginfo('url') . '/wp-login.php');
+}
+
+function is_file_secure($filename) {
+    global $wpdb;
+    
+    $sql = $wpdb->prepare("select COUNT(*) as count
+                            from wp_posts p
+                            inner join wp_posts a on p.ID = a.post_parent
+                            inner join wp_postmeta pm on a.ID = pm.post_id
+                                    and pm.meta_key = '_wp_attached_file'
+                            left join wp_pvs_user_item pvs on p.ID = pvs.object_id
+                            where 1=1
+                            and pvs.object_id is null
+                            and pm.meta_value = '$filename';");
+    
+    $count = $wpdb->get_var($sql);
+    
+    return $count > 0;
+    
 }
 
 
